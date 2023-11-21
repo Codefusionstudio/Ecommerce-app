4e39eb5c45dd7f1cf3a88022807355bf9cef709d
Codefusionstudio <144534559+Codefusionstudio@users.noreply.github.com>
2023-11-21 15:01:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 import path from 'path';
+import {fileURLToPath} from 'url';
 
 //configure env
 dotenv.config();
@@ -16,6 +17,9 @@ dotenv.config();
 //databse config
 connectDB();
 
+// es filemodulefix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__dirname);
 //rest object
 const app = express();
 
